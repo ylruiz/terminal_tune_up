d1a40a5c96deb70e618e8848cc49579801c7f2dd
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2024-03-01 15:17:46 +0100

    feat(TX-3): Adjust _GitOperator operations

--- bin/git_operator.dart
+++ bin/git_operator.dart
@@ -3,27 +3,30 @@ part of 'target_operator.dart';
 class _GitOperator {
   static List<String> get changedFilesExcludingDeletedFiles {
     log('Getting changed files...');
-    return Process.runSync(
+    final changedFiles = Process.runSync(
       'git',
       [
         'diff',
         '--name-only',
-        '--diff-filter=d',
         'main...',
       ],
     ).stdout.split('\n');
+
+    log('Excluding deleted files from changed files...');
+    return Process.runSync('git', ['ls-files', ...changedFiles])
+        .stdout
+        .split('\n')
+        .where((file) => !file.startWith('D'))
+        .toList();
   }
 
   static List<String> get unchangedFiles {
     log('Getting unchanged files...');
-    return Process.runSync(
-      'git',
-      [
-        'diff',
-        '--name-only',
-        '--diff-filter=U',
-        'main...',
-      ],
-    ).stdout.split('\n');
+
+    final allFiles =
+        Process.runSync('git', ['ls-files']).stdout.split('\n').toList();
+    final filteredFiles = changedFilesExcludingDeletedFiles;
+
+    return allFiles.where((file) => !filteredFiles.contains(file)).toList();
   }
 }
